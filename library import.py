58cd3d9f766203c9144041d05d6dce721f80e2a6
ddcottontwo <ddcottontwo@gmail.com>
2020-03-07 14:58:38 -0500

    2/7 3pm

--- library import.py
+++ library import.py
@@ -4,6 +4,6 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
-#url set & ping
+#url set & ping (here's an example output... will need to automate collection)
 url = 'https://www.genome.jp/dbget-bin/www_bget?sce:YKL127W+sce:YMR105C+sce:YMR278W'
 response = requests.get(url)
